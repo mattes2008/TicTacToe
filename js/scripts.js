86398268644a35b34a9ce86e2027a74d7310a2ac
mattes2008 <mattes2906.dev@gmail.com>
2022-09-19 21:49:53 +0200

    added full-game-HTML-file

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,13 @@ let data = {
     sp1: {
 
     	mode: "player",
-    	name: "Spieler 1"
+    	name: "player 1",
 
     },
     sp2: {
 
     	mode: "player",
-    	name: "Spieler 2"
+    	name: "player 2",
 
     },
 
@@ -37,7 +37,6 @@ function bodyOnLoad () {
 function checkForWin (src) {
 
     src = src.split("")
-    let constal
 
     for (let i=0; i<data.winning.length; i++) {
 
@@ -57,34 +56,51 @@ function checkForWin (src) {
         }
 
         if (winningPoints1===3) {
-            return "sp1"
+
             for (let i3=0; i3<3; i3++) {
 
                 data.elements[data.winning[i][i3]].setAttribute ("style", "background-color: yellow;")
 
             }
+
+            for (let i4=0; i4<data.elements.length; i4++) {
+
+            	data.elements[i4].setAttribute("onclick", "")
+
+            }
+
+            document.getElementById("displayText").innerHTML = "&#x1F451;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;Congratulations, " + data.sp1.name + "! You won!"
+            document.getElementById("display").setAttribute ("style", "background-color: yellow; border: thin solid #DFD800;")
+
+            return "sp1"
 
         } else if (winningPoints2===3) {
-            return "sp2"
             for (let i3=0; i3<3; i3++) {
 
                 data.elements[data.winning[i][i3]].setAttribute ("style", "background-color: yellow;")
 
             }
+
+            for (let i4=0; i4<data.elements.length; i4++) {
+
+            	data.elements[i4].setAttribute("onclick", "")
+
+            }
+
+            document.getElementById("displayText").innerHTML = "&#x1F451;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;Congratulations, " + data.sp2.name + "! You won!"
+			document.getElementById("display").setAttribute ("style", "background-color: yellow; border: thin solid #DFD800;")
+
+            return "sp2"
 
         } else if (i===data.winning.length-1) {
             let remis = checkForRemis(src)
             if (remis) {
+            	document.getElementById("displayText").innerHTML = "<b>=</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Remis, click here to restart!"
+            	document.getElementById("display").setAttribute ("style", "background-color: green; border: thin solid #007F00;")
                 return "remis"
             } else {
                 return false
             }
-        }
-
-        for (let i3=0; i3<3; i3++) {
-
-
-
         }
 
     }
@@ -118,6 +134,9 @@ function choseField (element, player) {
         element.innerHTML = "X"
         element.setAttribute ("style", "color: red;")
         data.activePlayer = "sp2"
+        element.setAttribute("onclick", "")
+        document.getElementById("displayText").innerHTML = "<b>O</b>&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;" + data.sp2.name + ", it's your turn!"
+		document.getElementById("display").setAttribute ("style", "background-color: blue; border: thin solid #00007F;")
         parseField()
 
     } else if (player==="sp2") {
@@ -125,6 +144,9 @@ function choseField (element, player) {
         element.innerHTML = "O"
         element.setAttribute ("style", "color: blue;")
         data.activePlayer = "sp1"
+        element.setAttribute("onclick", "")
+        document.getElementById("displayText").innerHTML = "<b>X</b>&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;" + data.sp1.name + ", it's your turn!"
+		document.getElementById("display").setAttribute ("style", "background-color: red; border: thin solid #7F0000;")
         parseField()
 
     }
@@ -166,5 +188,57 @@ function parseField () {
 	}
 
 	checkForWin (str)
+
+}
+
+
+
+
+
+function hideSettings () {
+
+	document.getElementById("settings").classList.toggle("hidden")
+
+}
+
+
+
+
+function selectSettings () {
+
+	if (document.getElementById("nameInput1").value==="") {
+		data.sp1.name = "player 1"
+	} else {
+		data.sp1.name = document.getElementById("nameInput1").value
+	}
+
+	if (document.getElementById("nameInput2").value==="") {
+		data.sp2.name = "player 2"
+	} else {
+		data.sp2.name = document.getElementById("nameInput2").value
+	}
+
+	data.sp1.mode = document.getElementById("modeInput1").value
+	data.sp2.mode = document.getElementById("modeInput2").value
+
+}
+
+
+
+
+function restart () {
+
+	data.activePlayer = "sp1"
+
+	for (let i=0; i<data.elements.length; i++) {
+
+		data.elements[i].innerHTML = ""
+		data.elements[i].setAttribute ("style", "")
+		data.elements[i].setAttribute ("onclick", "playerChose (this)")
+
+	}
+
+	document.getElementById("displayText").innerHTML = "<b>X</b>&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;" + data.sp1.name + ", it's your turn!"
+	document.getElementById("display").setAttribute ("style", "background-color: red; border: thin solid #7F0000;")
 
 }
